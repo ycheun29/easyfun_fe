bdd435514c8b031f39c43a2b18e042835d4a3813
Ines <leeyfan@gmail.com>
2023-03-13 16:07:53 -0400

    Creat login and signup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { IndexComponent } from './components/index.component';
 import { ActivityListComponent } from './components/activity/list.component';
+import { LoginComponent } from './components/user/login.component';
+import { SignupComponent } from './components/user/signup.component';
 
 @NgModule({
   imports: [RouterModule.forRoot([
     { path: "", component: IndexComponent },
     { path: "activity/list", component: ActivityListComponent },
-    { path: "**", redirectTo: "" }
+    { path: "user/login", component: LoginComponent},
+    { path: "user/signup", component: SignupComponent},
+    { path: "**", redirectTo: "" },
 ])],
   exports: [RouterModule]
 })
