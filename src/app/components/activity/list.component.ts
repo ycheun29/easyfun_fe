4f4fed20b7224e469bbf76d56b12cedf9b5251ed
kityiwan <kityi.wan.emily@gmail.com>
2023-03-18 16:19:34 -0400

    EAS-12 Sort By Button

--- src/app/components/activity/list.component.ts
+++ src/app/components/activity/list.component.ts
@@ -15,6 +15,7 @@ export class ActivityListComponent{
     filteredActivities: Activity[] = [];
     keyword = '';
     selectedCategory = 'all';
+    selectedSort = 'dateAsc';
 
     constructor(public repository: ActivityRepository, private router: Router, private cdRef: ChangeDetectorRef) {
         this.repository.setActivityList();
@@ -33,17 +34,73 @@ export class ActivityListComponent{
     searchActivities(): void {
         if (this.selectedCategory === 'all'){
             this.filteredActivities = this.activityList.filter(item => item.title && item.title.toLowerCase().includes(this.keyword.toLowerCase()));
-            console.log(this.filteredActivities);
         }
         else {
             this.filteredActivities = this.activityList.filter(item => item.title && item.title.toLowerCase().includes(this.keyword.toLowerCase())).filter(item => item.category === this.selectedCategory);
-            console.log(this.filteredActivities);
+        }
+        switch (this.selectedSort){
+            case 'dateAsc':
+                this.filteredActivities.sort((a, b) => {
+                    const aDate = a.date ? new Date(a.date) : undefined;
+                    const bDate = b.date ? new Date(b.date) : undefined;
+                    
+                    if (aDate && bDate) {
+                      return aDate.getTime() - bDate.getTime();
+                    } else if (bDate) {
+                      return -1;
+                    } else if (aDate) {
+                      return 1;
+                    } else {
+                      return 0;
+                    }
+                  });
+                break;
+            case 'dateDes':
+                this.filteredActivities.sort((a, b) => {
+                    const aDate = a.date ? new Date(a.date) : undefined;
+                    const bDate = b.date ? new Date(b.date) : undefined;
+                        
+                    if (aDate && bDate) {
+                        return bDate.getTime() - aDate.getTime();
+                    } else if (aDate) {
+                        return -1;
+                    } else if (bDate) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                  });
+                break;
+                case 'priceAsc':
+                    this.filteredActivities.sort((a, b) => {
+                        if (a.price !== undefined && b.price !== undefined) {
+                          return a.price - b.price;
+                        }
+                        // Return 0 if either a.price or b.price is undefined
+                        return 0;
+                      });
+                    break;
+                case 'priceDes':
+                    this.filteredActivities.sort((a, b) => {
+                        if (a.price !== undefined && b.price !== undefined) {
+                          return b.price - a.price;
+                        }
+                        // Return 0 if either a.price or b.price is undefined
+                        return 0;
+                      });
+                    break;    
         }
         this.cdRef.detectChanges();
+        console.log(this.filteredActivities);
     }
 
     onCategorySelected(category: string): void {
         this.selectedCategory = category;
+        this.searchActivities();
+    }
+    
+    onSortSelected(sort: string): void {
+        this.selectedSort = sort;
         this.searchActivities();
     }
 }
