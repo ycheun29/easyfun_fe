9c8aa816b457336ec47b60d84a09e03a7a77e2cd
kityiwan <kityi.wan.emily@gmail.com>
2023-03-18 15:23:51 -0400

    EAS-12 Filter By

--- src/app/components/activity/list.component.ts
+++ src/app/components/activity/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ChangeDetectorRef } from "@angular/core";
 import { Router } from "@angular/router";
+
 import { Activity } from "../../models/activity.model";
 import { ActivityRepository } from "../../models/activity.repository";
 
@@ -13,8 +14,9 @@ export class ActivityListComponent{
     title = 'Explore Activities';
     filteredActivities: Activity[] = [];
     keyword = '';
+    selectedCategory = 'all';
 
-    constructor(public repository: ActivityRepository, private router: Router) {
+    constructor(public repository: ActivityRepository, private router: Router, private cdRef: ChangeDetectorRef) {
         this.repository.setActivityList();
     }
     formatDate(date: Date | undefined): string {
@@ -29,9 +31,19 @@ export class ActivityListComponent{
     }
 
     searchActivities(): void {
-        this.filteredActivities = this.activityList.filter(item => item.title && item.title.toLowerCase().includes(this.keyword.toLowerCase()));
-        console.log(this.filteredActivities);
+        if (this.selectedCategory === 'all'){
+            this.filteredActivities = this.activityList.filter(item => item.title && item.title.toLowerCase().includes(this.keyword.toLowerCase()));
+            console.log(this.filteredActivities);
+        }
+        else {
+            this.filteredActivities = this.activityList.filter(item => item.title && item.title.toLowerCase().includes(this.keyword.toLowerCase())).filter(item => item.category === this.selectedCategory);
+            console.log(this.filteredActivities);
+        }
+        this.cdRef.detectChanges();
     }
 
-
+    onCategorySelected(category: string): void {
+        this.selectedCategory = category;
+        this.searchActivities();
+    }
 }
